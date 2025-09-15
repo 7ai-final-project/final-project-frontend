afad5ac39bdd35f642e25ac2e0be552f7c2a0854
queenjerry614 <queenjerry614@gmail.com>
2025-09-15 15:47:17 +0900

    minor change2

--- components/game/SingleModeGame.tsx
+++ components/game/SingleModeGame.tsx
@@ -381,7 +381,7 @@ const styles = StyleSheet.create({
   },
  imageContainer: {
     width: "100%",
-    height: 800,
+    height: 700,
     backgroundColor: "#20232a",
     borderRadius: 15,
     justifyContent: "center",
